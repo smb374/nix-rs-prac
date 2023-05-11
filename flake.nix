b1defd828c27a57eeda335cfc995a0570614695c
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-05-11 15:07:33 +0800

    Finish integration with nci.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+    nci.url = "github:yusdacra/nix-cargo-integration";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
     nix2container.url = "github:nlewo/nix2container";
     nix2container.inputs.nixpkgs.follows = "nixpkgs";
     mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
@@ -13,23 +15,34 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
+        inputs.nci.flakeModule
       ];
       systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+      let
+        crateName = "nix-rs-prac";
+        crateOutputs = config.nci.outputs.${crateName};
+      in {
+        ### nci
+        nci.projects.${crateName}.relPath = "";
+        nci.crates.${crateName} = {
+          export = true;
+        };
+        ### devenv
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
+        packages.default = crateOutputs.packages.release;
 
         devenv.shells.default = {
           name = "my-project";
 
           # https://devenv.sh/reference/options/
           packages = with pkgs; [
-            config.packages.default
+            hello
             git
           ];
 
